c46718e9ec50fc04a606e8b33d092c3ee73ef617
Abram Hindle <abram.hindle@softwareprocess.es>
2022-08-11 10:48:20 -0600

    Missing TTWL DB Schema

---
+++ Apocalyptech/dataprocessing/refs_db_schema.sql
@@ -0,0 +1,19 @@
+drop table if exists ttwlobject;
+drop table if exists ttwlrefs;
+
+create table ttwlobject
+(
+    id int not null auto_increment,
+    name varchar(512) character set latin1 not null,
+    primary key (id),
+    unique index idx_name (name)
+) engine=innodb;
+
+create table ttwlrefs
+(
+    from_obj int not null,
+    to_obj int not null,
+    unique index idx_from (from_obj, to_obj),
+    index idx_to (to_obj, from_obj)
+) engine=innodb;
+
