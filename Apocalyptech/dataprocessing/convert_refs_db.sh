c8ad1fc357a738a27894aee010c1753b81383842
Abram Hindle <abram.hindle@softwareprocess.es>
2022-08-11 10:42:59 -0600

    Genericifying the extraction---db extract settings

--- Apocalyptech/dataprocessing/convert_refs_db.sh
+++ Apocalyptech/dataprocessing/convert_refs_db.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 # vim: set expandtab tabstop=4 shiftwidth=4:
-
-read -sp 'Enter wlrefs pass: ' FULLPASS
-read -sp 'Enter wlrefs_ro pass: ' PASS
+# read -sp 'Enter wlrefs pass: ' FULLPASS
+# read -sp 'Enter wlrefs_ro pass: ' PASS
+echo "Reading settings from local-settings-rc"
+source local-settings-rc
 echo
 echo "Dumping SQL first..."
 echo
 rm wlrefs.zip
-mysqldump -u wlrefs -p${FULLPASS} -h mcp wlrefs > wlrefs.sql
+# Might have to make -p optional
+mysqldump -u ${USER} -p${FULLPASS} -h${HOST} -p{$PORT} wlrefs > wlrefs.sql
 zip wlrefs.zip wlrefs.sql
 rm wlrefs.sql
 echo
 echo "Now doing SQLite dump/conversion..."
 echo
 rm wlrefs.sqlite3*
-/usr/local/dv/virtualenv/mysql2sqlite/bin/mysql2sqlite -f wlrefs.sqlite3 -d wlrefs -u wlrefs_ro -h mcp -V -p ${PASS} && zip wlrefs.sqlite3.zip wlrefs.sqlite3 && ls -lh wlrefs.sqlite3*
+# Might have to make -P optional
+${MYSQL2SQLITE} -f wlrefs.sqlite3 -d wlrefs -u ${USER} -h ${HOST} -P ${PORT} -V -p ${PASS} && zip wlrefs.sqlite3.zip wlrefs.sqlite3 && ls -lh wlrefs.sqlite3*
 ls -lh wlrefs.zip
